84f2276042a458e18f48ef6498fcef9d60c75ecb
tobia <tobia.dipisa@geo-solutions.it>
2011-07-26 12:13:16 +0200

    Cleaning upload form

--- app/static/script/app/GeoExplorer/Composer.js
+++ app/static/script/app/GeoExplorer/Composer.js
@@ -422,7 +422,8 @@ GeoExplorer.Composer = Ext.extend(GeoExplorer, {
                         handler: function(){
                             if(fp.getForm().isValid()){
                               fp.getForm().submit({
-                                  url: app.proxy + app.xmlJsonTranslateService + 'HTTPWebGISFileUpload',
+                                  url: app.xmlJsonTranslateService + 'HTTPWebGISFileUpload',
+                                  //url: app.proxy + app.xmlJsonTranslateService + 'HTTPWebGISFileUpload',
                                   waitMsg: 'Uploading your context file...',
                                   success: function(fp, o){
                                       win.hide();
