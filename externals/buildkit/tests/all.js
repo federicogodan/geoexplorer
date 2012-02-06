17a04ff162c515444d1afe535065750e07737c50
tobia <tobia.dipisa@geo-solutions.it>
2012-02-06 16:13:21 +0100

    Removing old dependencies

--- externals/buildkit/tests/all.js
+++
@@ -1,6 +0,0 @@
-exports["test: buildkit"] = require("./test_buildkit");
-
-if (require.main == module || require.main == module.id) {
-    require("test").run(exports);
-}
-
--- externals/buildkit/tests/test_buildkit.js
+++
@@ -1,5 +0,0 @@
-exports["test: merge"] = require("./buildkit/test_merge");
-
-if (require.main == module || require.main == module.id) {
-    require("test").run(exports);
-}
