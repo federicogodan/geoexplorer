bf1f92cae65db8d8b71ff4822c06afc3ce80a15f
tobia <tobia.dipisa@geo-solutions.it>
2012-04-17 11:21:07 +0200

    Introducing i18n management

--- app/static/translations/fr.js
+++ app/static/translations/fr.js
@@ -1,6 +1,10 @@
 /**
  * Copyright (c) 2009-2010 The Open Planning Project
  */
+/*
+	NOTE: configuration customization could override
+	these strings
+*/
 
 GeoExt.Lang.add("fr", {
     "GeoExplorer.prototype": {
@@ -14,19 +18,27 @@ GeoExt.Lang.add("fr", {
         saveErrorText: "Problème de sauvegarde",
         bookmarkText: "Mettre l`URL en favori",
         permakinkText: "Permalien",
-        appInfoText: "GeoExplorer",
+        appInfoText: "Crédits",
         aboutText: "À propos de GeoExplorer",
         mapInfoText: "Info cartographique",
         descriptionText: "Description",
         contactText: "Contact",
         aboutThisMapText: "À propos de cette carte",
+		resetButtonTooltip: "Réinitialiser la Page",
+		helpButtonTooltip: "Aide",
         searchTabTitle : "Chercher",
-        viewTabTitle : "Vue"
+        viewTabTitle : "Vue",
+        portalTabTitle : "Portail"
     },  
 
     "GeoExplorer.Composer.prototype": {
-        loadMapText: "Importer une carte",
+        loadMapText: "Importer une carte ou un fichier KML",
         saveMapText: "Exporter une carte",
+		loadMapEmptyText: "Sélectionnez un fichier KML ou de contexte",
+		loadMapUploadText: "Chargement...",
+		uploadText: "Télécharger",
+		loadMapWindowTitle: 'formulaire de téléchargement de  fichier',
+		loadMapErrorText:'Erreur de chargement de fichier',
         toolsTitle: "Sélectionner l`application à inclure dans la barre d`outils",
         previewText: "Previsualiser",
         backText: "Retour en arrière",
@@ -34,7 +46,8 @@ GeoExt.Lang.add("fr", {
         loginText: "Se connecter",
         loginErrorText: "Nom d`utilisateur ou mot de passe incorrect",
         userFieldText: "Utilisateur",
-        passwordFieldText: "Mot de passe"
+        passwordFieldText: "Mot de passe",
+        fullScreenText: "Plein écran"
     },  
 
     "gxp.menu.LayerMenu.prototype": {
@@ -45,23 +58,26 @@ GeoExt.Lang.add("fr", {
         addActionMenuText: "Ajouter des couvertures",
         addActionTip: "Ajouter des couvertures",
         addServerText: "Ajouter un nouveau serveur",
-        addButtonText: "",
+        addButtonText: "Ajouter des couvertures",
         untitledText: "Sans titre",
         addLayerSourceErrorText: "Erreur d`obtention des capacités WMS ({msg}).\nVeuillez vérifier l`URL et essayez à nouveau.",
         availableLayersText: "Couvertures disponibles",
-        expanderTemplateText: "<p><b>Résumé:</b> {résumé}</p>",
+        expanderTemplateText: "<p><b>Résumé:</b> {abstract}</p>",
         panelTitleText: "Titre",
         layerSelectionText: "Visualiser les données disponibles depuis:",
         doneText: "Terminé",
+        removeFilterText: "effacer le filtre", 
+        filterEmptyText: "Filtre",
         uploadText: "Télécharger les données"
     }, 
-
-    "gxp.plugins.RemoveOverlays.prototype": {
-	    removeOverlaysMenuText: "Remove overlays",
-	    removeOverlaysActionTip: "Removes all overlays from the map",
-	    removeOverlaysConfirmationText: "Are you sure you want to remove all loaded overlays from the map?",
-    }, 
-
+	
+	"gxp.plugins.RemoveOverlays.prototype": {
+	    removeOverlaysMenuText: "Retirer des superpositions",
+	    removeOverlaysActionTip: "Supprime toutes les superpositions de la carte",
+	    removeOverlaysConfirmationText: "Etes-vous sûr de vouloir supprimer tous les calques chargés de la carte?"
+    },
+    
+	
     "gxp.plugins.BingSource.prototype": {
         title: "Bing couvertures",
         roadTitle: "Bing routes",
@@ -95,7 +111,7 @@ GeoExt.Lang.add("fr", {
     },  
 
     "gxp.plugins.Legend.prototype": { 
-        menuText: "Montrer la légende",
+        menuText: "Légende",
         tooltip: "Montrer la légende"
     },  
 
@@ -198,5 +214,106 @@ GeoExt.Lang.add("fr", {
 
     "gxp.ScaleOverlay.prototype": { 
         zoomLevelText: "Niveau de zoom"
+    },
+    "gxp.plugins.AddGroup.prototype": { 
+	    addGroupMenuText: "Ajouter un Groupe",
+	    addGroupActionTip: "Ajoutez un nouveau groupe à l'arbre des couvertures.",   
+	    addGroupDialogTitle: "Nouveau Groupe", 
+	    addGroupFieldSetText: "Groupe Nom",
+	    addGroupFieldLabel: "Nouveau Groupe",
+	    addGroupButtonText: "Ajouter un Groupe",
+	    addGroupMsg: "Entrez le nom du groupe"	
+    },
+    
+    "gxp.plugins.RemoveGroup.prototype": { 
+	    removeGroupMenuText: "Supprimer le groupe",
+	    removeGroupActionTip: "Supprimer le groupe de l'arbre des couvertures",
+	    removeGroupActionTip: "Supprimer le groupe sélectionné et ses couvertures de la carte",
+	    removeGroupConfirmationText: "Vous êtes certain que vous souhaitez supprimer le groupe ? Tous les couvertures au sein du groupe seront supprimés de la carte."
+    },
+    
+    "gxp.plugins.SaveDefaultContext.prototype": { 
+	    saveDefaultContextMenuText: "Enregistrez le contexte par défaut.",
+	    saveDefaultContextActionTip: "Enregistrez le contexte actuel.",
+	    contextSaveSuccessString: "Sauver le contexte est géré.",
+	    contextSaveFailString: "Sauver le contexte a échoué."
+    },
+    "gxp.plugins.FDHGeoCoder.prototype":{
+        initialText: "Sélectionner une zone",
+        menuText: "FDH Codification Géographique",
+        tooltip: "FDH Codification Géographique"
+	
+    },
+	"gxp.plugins.ZoomBox.prototype":{
+        zoomInMenuText: "Zoom Box Avant",
+        zoomOutMenuText: "Zoom Box Arrière",
+        zoomInTooltip: "Zoom Box Avant",
+        zoomOutTooltip: "Zoom Box Arrière"
+    },
+    "GeoExt.ux.PrintPreview.prototype":{
+        paperSizeText: "Taille de papier:",
+        resolutionText: "Résolution:",
+        printText: "Imprimer",
+        emptyTitleText: "Entrez le titre de la carte ici.",
+        includeLegendText: "Inclure la légende?",
+        legendOnSeparatePageText: "Légende sur une page distincte?",
+        compactLegendText: "Légende compact?",	
+        emptyCommentText: "Entrer des commentaires d'ici.",
+        creatingPdfText: "Création PDF..."
+    },
+    "gxp.plugins.GeonetworkSearch.prototype":{
+        geonetworkSearchText: "Afficher les métadonnées",
+        geonetworkSearchActionTip: "Afficher les métadonnées"
+    },
+    "gxp.plugins.GroupProperties.prototype":{
+        groupPropertiesMenuText: "Propriétés du groupe",
+        groupPropertiesActionTip: "Propriétés du groupe",
+        groupPropertiesDialogTitle: "Propriétés du groupe - ",
+        groupPropertiesFieldSetText: "Nom du groupe",
+        groupPropertiesFieldLabel: "nom du nouveau groupe",
+        groupPropertiesButtonText: "Fait",
+        groupPropertiesMsg: "Veuillez entrer un nom de groupe"
+    },
+    
+    "gxp.plugins.Login.prototype":{
+      loginText: "Se connecter",
+          loginErrorText: "Nom d`utilisateur ou mot de passe incorrect",
+          userFieldText: "Utilisateur",
+          passwordFieldText: "Mot de passe"
+    },
+	
+	"gxp.plugins.FeatureGrid.prototype": {
+        displayFeatureText: "Afficher sur la carte",
+        firstPageTip: "Première page",
+        previousPageTip: "Page précédente",
+        zoomPageExtentTip: "Zoom sur la page",
+        nextPageTip: "Page suivante",
+        nextPageTip: "Dernière page",
+        totalMsg: "Total: {0} records"
+    },
+	
+	"gxp.plugins.QueryForm.prototype": {
+        queryActionText: "Query",
+        queryMenuText: "Couche de requêtes",
+        queryActionTip: "Interroger la couche sélectionnée",
+        queryByLocationText: "Query selon le lieu",
+        currentTextText: "Mesure actuelle",
+        queryByAttributesText: "Requête par attributs",
+		queryMsg: "Recherche...",
+        cancelButtonText: "Remettre",
+        noFeaturesTitle: "Pas de correspondance",
+        noFeaturesMessage: "Votre requête n'a retourné aucun résultat.",
+        title: "Rechercher",
+        northLabel:"Nord",
+        westLabel:"Ouest",
+        eastLabel:"Est",
+        southLabel:"Sud",
+        setAoiText: "FixéROI",
+        setAoiTooltip: "Activer le contrôle TextBox pour dessiner un ROI (BOX) sur la carte",
+        attributeEnablement: "Requête par attributs",
+        attributeEnablementMsg: "Invalid search Type! To use this you have to select 'Feature' type and to select a vector layer before.",
+        searchType: "Réglages de base",
+        typeLabel: "Type",
+        featureLabel: "Max Features"
     }
 });
