83c2533415bddf0cdf2254019cd0396d554ca501
Riccardo Mari <riccardo_mari@tiscali.it>
2012-05-31 10:06:29 +0200

    change ../templates/custom.html in ../templates/composer.html

--- app/root/index.js
+++ app/root/index.js
@@ -22,6 +22,6 @@ var auth = require("../auth");
 exports.app = function(req) {
     var request = new Request(req);
     var details = auth.getDetails(request);
-    var response = Response.skin(module.resolve("../templates/custom.html"), {status: details.status || 404});
+    var response = Response.skin(module.resolve("../templates/composer.html"), {status: details.status || 404});
     return response;
 };
