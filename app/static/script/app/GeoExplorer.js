30b7cb5ce2a52ca98d120cb4bbe6bf4cc50b3f04
Riccardo Mari <riccardo_mari@tiscali.it>
2012-06-01 16:18:08 +0200

    add fix slider IE

--- app/static/script/app/GeoExplorer.js
+++ app/static/script/app/GeoExplorer.js
@@ -4,6 +4,36 @@
 
 Ext.USE_NATIVE_JSON = true;
 
+// No shadows for windows, so they can be resized without the need to also
+// resize the shadow.
+// TODO remove when we have switched to CrumbPanel navigation.
+Ext.Window.prototype.shadow = false;
+
+// http://www.sencha.com/forum/showthread.php?141254-Ext.Slider-not-working-properly-in-IE9
+// TODO re-evaluate once we move to Ext 4
+Ext.override(Ext.dd.DragTracker, {
+    onMouseMove: function (e, target) {
+        if (this.active && Ext.isIE && !Ext.isIE9 && !e.browserEvent.button) {
+            e.preventDefault();
+            this.onMouseUp(e);
+            return;
+        }
+        e.preventDefault();
+        var xy = e.getXY(), s = this.startXY;
+        this.lastXY = xy;
+        if (!this.active) {
+            if (Math.abs(s[0] - xy[0]) > this.tolerance || Math.abs(s[1] - xy[1]) > this.tolerance) {
+                this.triggerStart(e);
+            } else {
+                return;
+            }
+        }
+        this.fireEvent('mousemove', this, e);
+        this.onDrag(e);
+        this.fireEvent('drag', this, e);
+    }
+});
+
 (function() {
     // backwards compatibility for reading saved maps
     // these source plugins were renamed after 2.3.2
@@ -195,12 +225,12 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
 
     loadConfig: function(config) {
 
-        /*if(config.isLoadedFromConfigFile){
+        if(config.isLoadedFromConfigFile){
           this.applyConfig(config);
         } else {
             Ext.Ajax.request({
-               //url: proxy + geoStoreBaseURL + "data/" + this.mapId,
-               url: geoStoreBaseURL + "data/" + this.mapId,
+               url: proxy + geoStoreBaseURL + "data/" + this.mapId,
+               //url: geoStoreBaseURL + "data/" + this.mapId,
                method: 'GET',
                scope: this,
                headers:{
@@ -230,8 +260,8 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
                   this.applyConfig(config);
                }
             });        
-        }*/
-
+        }
+        /*
         var success = function(request) {                                
                   var addConfig;
                   try {
@@ -260,6 +290,7 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
           failure: failure,
           scope: this
         });
+        */
         
     },
     
@@ -275,14 +306,16 @@ var GeoExplorer = Ext.extend(gxp.Viewer, {
         app.destroy();
 
         var config = Ext.util.JSON.decode(json);
-        if(config && config.result.map){
+        //if(config && config.result.map){
+        if(config && config.map){
             config.isLoadedFromConfigFile = true;
             
             //if(modified){
             //    config.modified = modified;
             //}
             
-            app = new GeoExplorer.Composer(config);
+            //app = new GeoExplorer.Composer(config);
+            app = new GeoExplorer.Composer(config, this.mapId, this.auth, this.fScreen);
         }else{
             Ext.Msg.show({
                 title: this.userConfigLoadTitle,
