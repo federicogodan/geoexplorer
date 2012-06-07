4018d0c87de45c1b516f952a5103327fc09a4717
tobia <tobia.dipisa@geo-solutions.it>
2012-06-07 19:45:34 +0200

    Several fixes for URL parameters and viewer config

--- app/static/urlConfig.js
+++ app/static/urlConfig.js
@@ -1,6 +1,6 @@
 {
-   "geoStoreBase":"http://192.168.1.132:8080/geostore/rest/",
-   "proxy":"/http_proxy/proxy/?url=",
+   "geoStoreBase":"http://localhost:8080/geostore/rest/",
+   "proxy":"/proxy/?url=",
    "gsSources":[
    ]
 }
