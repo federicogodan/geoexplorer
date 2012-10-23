7ebc90641f68b9a59ade0b37ccc21a8c62ec9438
Riccardo Mari <riccardo_mari@tiscali.it>
2012-10-23 17:22:35 +0200

    change credits and layer properties tab for Mammal DB and Habitat plugin

--- app/static/urlConfig.js
+++ app/static/urlConfig.js
@@ -1,6 +1,6 @@
 {
-    "geoStoreBase":"http://localhost:8080/geostore/rest/",
-    "proxy":"/proxy/?url=",
+   "geoStoreBase":"http://localhost:8080/geostore/rest/",
+   "proxy":"/proxy/?url=",
     "gsSources":[],
     "scaleOverlayUnits":{
         "topOutUnits":"nmi",    
