088911c47e1bfb8c093c19ab3340e5a91960b097
Andrea Marongiu <>
2012-11-28 15:43:22 +0100

    openalyer Extension for WPSManager plugin added

---
+++ app/static/externals/openlayersExt/lib/OpenLayers/GeoStore.js
@@ -0,0 +1,113 @@
+
+/**
+ * Class: OpenLayers.Format.GeoStore.Category
+ * GeoStore Entity Category.
+ */
+OpenLayers.GeoStore.Category = OpenLayers.Class({ 
+    
+    /**
+     * Property: type
+     * {String} Geostore entity type
+     */
+    type: "category",
+    
+    /**
+     * Property: name
+     * {String} Category name
+     */
+    name: null,
+    
+    
+    initialize: function(options) {
+        OpenLayers.Util.extend(this, options);
+    },
+    
+    CLASS_NAME: "OpenLayers.GeoStore.Category"
+    
+});
+
+
+
+
+/**
+ * Class: OpenLayers.Format.GeoStore.User
+ * GeoStore Entity User.
+ */
+OpenLayers.GeoStore.User = OpenLayers.Class({ 
+    
+    /**
+     * Property: type
+     * {String} Geostore entity type
+     */
+    type: "user",
+    
+    /**
+     * Property: name
+     * {String} User name
+     */
+    name: null,
+    
+    
+    initialize: function(options) {
+        OpenLayers.Util.extend(this, options);
+    },
+    
+    CLASS_NAME: "OpenLayers.GeoStore.User"
+    
+});
+
+
+
+
+/**
+ * Class: OpenLayers.GeoStore.Resource
+ * GeoStore Entity Resource.
+ */
+OpenLayers.GeoStore.Resource = OpenLayers.Class({ 
+    
+    /**
+     * Property: type
+     * {String} Geostore entity type
+     */
+    type: "resource",
+    
+    /**
+     * Property: name
+     * {String} Resource name
+     */
+    name: null,
+    
+    /**
+     * Property: Category name
+     * {String} Resource category
+     */
+    category: null,
+    
+    /**
+     * Property: metadata
+     * {String} Metadata String (Blob representation)
+     */
+    metadata: null,
+    
+    
+    /**
+     * Property: attributes
+     * {String} Metadata String (Blob representation)
+     */
+    attributes: null,
+    
+    
+    /**
+     * Property: store
+     * {String} Store String (Blob representation)
+     */
+    store: null,
+    
+    
+    initialize: function(options) {
+        OpenLayers.Util.extend(this, options);
+    },
+    
+    CLASS_NAME: "OpenLayers.Resource"
+    
+});
