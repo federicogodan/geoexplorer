2b559f0029da25219e685a8f11c71e652c350a57
Andrea Marongiu <>
2012-11-29 14:07:51 +0100

    WFSGrid plugin properties added

--- app/static/urlConfig.js
+++ app/static/urlConfig.js
@@ -10,17 +10,19 @@
         "bottomOutUnits":"km"
     },
     "spm": {
-        "wpsURL": "http://84.33.1.254:8080/geoserver/wps",
+        "wpsURL": "http://84.33.1.254/geoserver/wps",
         "octaveExecutablePath":"/usr/bin/octave-3.2.4",
         "octaveConfigFilePath":"/data/NURC-IDA/matlabcode2/",
         "userId":"afabiani",
-        "outputUrl":"http://84.33.1.254:8080/geoserver",
+        "outputUrl":"http://84.33.1.254/geoserver/ows",
         "srcPath": "the_path",
         "itemStatus": "CREATED",
         "itemStatusMessage" : "Created by GUI",
         "wsName" : "nurc",
         "storeName" : "nurcdb",
-        "layerName" : "none"
+        "layerName" : "none",
+        "wfsURL": "http://84.33.1.254/geoserver/wfs",
+        "wfsFeature": "nurc:IDASoundPropModel"
     },
     "geostore": {
         "url": "http://localhost:8080/geostore/rest/",
