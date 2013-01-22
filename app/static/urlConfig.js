67656aaabc120719b3e64ecda761475d6d8346b4
Andrea Marongiu <>
2013-01-22 10:36:16 +0100

    Move dedicated plugin configuration form Composer to urlConfig

--- app/static/urlConfig.js
+++ app/static/urlConfig.js
@@ -1,6 +1,6 @@
  {
-    "geoStoreBase":"http://192.168.24.129:8080/geostore/rest/", 
-    "xmlJsonTranslateService": "http://192.168.24.129:8080/xmlJsonTranslate/",
+    "geoStoreBase":"http://192.168.24.182:8080/geostore/rest/", 
+    "xmlJsonTranslateService": "http://192.168.24.182:8080/xmlJsonTranslate/",
    
     "proxy": "proxy?url=",
 	
@@ -14,14 +14,21 @@
     },
 	
     "spm": {
-        "wpsURL": "http://84.33.1.254/geoserver/wps",
+        "wpsURL": "http://84.33.2.26/geoserver/wps",
         "userId":"afabiani",
-        "outputUrl":"http://84.33.1.254/geoserver/ows",
+        "outputUrl":"http://84.33.2.26/geoserver/ows",
         "srcPath": "the_path",
         "itemStatus": "CREATED",
         "itemStatusMessage" : "Created by GUI",
         "wsName" : "nurc",
         "styleName" : "spm"
+    },
+    
+    "rasterAlgebra": {
+        "wpsURL": "http://84.33.2.26/geoserver/wps",
+        "wsName" : "nurc",
+        "outputUrl":"http://84.33.2.26/geoserver/ows",
+        "storeName" : "nurcdb"
     },
 	
     
@@ -29,20 +36,28 @@
                 "ptype": "gxp_uploader",
 		"id": "svpuploader",
                 "loadMsg": "Load Sound Velocity Profile ...",
-                "uploadServiceURL": "http://192.168.24.129:8080/xmlJsonTranslate/FileUploader?uplodDir=/home/maro/",
+                "uploadServiceURL": "http://192.168.24.182:8080/xmlJsonTranslate/FileUploader?uplodDir=/home/maro/",
                 "inputFiles": [{ "name": "svpFile", "emptyText": "Sound Velocity Profile File", "fieldLabel": "SVP File"}]
 	    },{
                 "ptype": "gxp_uploader",
 		"id": "spmlistuploader",
                 "loadMsg": "Load SPM List...",
-                "uploadServiceURL": "http://192.168.24.129:8080/xmlJsonTranslate/FileUploader?type=inline",
+                "uploadServiceURL": "http://192.168.24.182:8080/xmlJsonTranslate/FileUploader?type=inline",
                 "inputFiles": [{ "name": "spnListFile", "emptyText": "SPM List XML File", "fieldLabel": "SPM List File", "allowBlank": true}]
 	    },{
                 "ptype": "gxp_saveDefaultContext",
                 "actionTarget": {"target": "paneltbar", "index": 40},
-				"needsAuthorization": true
+		"needsAuthorization": true
             }, {
                 "ptype": "gxp_idaattribute",
+                "wpsManager": "wpsRasterAlgebra",
+                "wfsGrid" : "wfsAlgebraGridPanel",
+                "colorStyles": [
+                    "Red",
+                    "Blue",
+                    "Green",
+                    "Black"					
+                ],
                 "outputConfig": {
                     "id": "attributepanel"
                 },
@@ -69,23 +84,135 @@
             }, {
                 "ptype": "gxp_wpsmanager",
                 "id": "wpsSPM",
-                "url": "http://84.33.1.254/geoserver/wps",
-		"geostoreUrl": "http://192.168.24.129:8080/geostore/rest",
+                "url": "http://84.33.2.26/geoserver/wps",
+		"geostoreUrl": "http://192.168.24.182:8080/geostore/rest",
+		"geostoreUser": "admin",
+		"geostorePassword": "admin",
+		"geostoreProxy": "proxy"
+            },{
+                "ptype": "gxp_wpsmanager",
+                "id": "wpsRasterAlgebra",
+                "url": "http://84.33.2.26/geoserver/wps",
+		"geostoreUrl": "http://192.168.24.182:8080/geostore/rest",
 		"geostoreUser": "admin",
 		"geostorePassword": "admin",
 		"geostoreProxy": "proxy"
             },{
                 "ptype": "gxp_wfsgrid",
                 "addLayerTool": "addlayer",
+                "title": "Layer Attribute",
+		"id": "wfsAlgebraGridPanel",
+                "wfsURL": "http://84.33.2.26/geoserver/wfs",
+                "featureType": "nurc:IDARasterAlgebraProcess",
+                "featureNS": "", 
+                "pageSize": 10,
+                "srsName": "EPSG:4326", 
+                "version": "1.1.0", 
+                "outputTarget": "idalaylist",
+                "columns" : [{
+                                "header": "Model Status", 
+                                "dataIndex": "itemStatus",
+                                "sortable": true
+                            },{
+                                "header": "Attribute Name", 
+                                "dataIndex": "attributeName",
+                                "sortable": true
+                            },{
+                                "header": "Model Run Date", 
+                                "dataIndex": "runBegin",
+                                "xtype": "datecolumn", 
+                                "format": "Y-m-d H:i:s",
+                                "sortable": true
+                            },{
+                                "header": "Model End Date", 
+                                "dataIndex": "runEnd",
+                                "xtype": "datecolumn", 
+                                "sortable": true,
+                                "format": "Y-m-d H:i:s"
+                            }
+                ],
+                "actionColumns" : [ 
+                    {
+                     "type": "addLayer",
+                     "layerNameAttribute" : "layerName",
+                     "layerTitleAttribute" : "name",
+                     "wsNameAttribute" : "wsName",
+                     "wmsURLAttribute" : "outputUrl",
+                     "showEqualFilter":{
+                         "attribute": "itemStatus",
+                         "value": "COMPLETED"    
+                     }
+                    },
+                    {
+                     "type": "details",
+                     "layerTitleAttribute" : "attributeName"
+                    },
+                    {
+                     "type": "delete",
+                     "layerNameAttribute" : "layerName",
+                     "idAttribute" : "fid",
+                     "wsNameAttribute" : "wsName"
+                    }
+                ]
+                
+            },{
+                "ptype": "gxp_wfsgrid",
+                "addLayerTool": "addlayer",
                 "title": "SPM",
 		"id": "wfsGridPanel",
-                "wfsURL": "http://84.33.1.254/geoserver/wfs",
+                "wfsURL": "http://84.33.2.26/geoserver/wfs",
                 "featureType": "nurc:IDASoundPropModel",
                 "featureNS": "", 
                 "pageSize": 10,
                 "srsName": "EPSG:4326", 
                 "version": "1.1.0", 
-                "outputTarget": "idalaylist"
+                "outputTarget": "idalaylist",
+                "columns" : [{
+                                "header": "Model Status", 
+                                "dataIndex": "itemStatus",
+                                "sortable": true
+                            },{
+                                "header": "Model Name", 
+                                "dataIndex": "modelName",
+                                "sortable": true
+                            },{
+                                "header": "Model Run Date", 
+                                "dataIndex": "runBegin",
+                                "xtype": "datecolumn", 
+                                "format": "Y-m-d H:i:s",
+                                "sortable": true
+                            },{
+                                "header": "Model End Date", 
+                                "dataIndex": "runEnd",
+                                "xtype": "datecolumn", 
+                                "sortable": true,
+                                "format": "Y-m-d H:i:s"
+                            }
+                ],
+                "actionColumns" : [ 
+                    {
+                     "type": "addLayer",
+                     "layerNameAttribute" : "layerName",
+                     "layerTitleAttribute" : "name",
+                     "wsNameAttribute" : "wsName",
+                     "wmsURLAttribute" : "outputUrl",
+                     "showEqualFilter":{
+                         "attribute": "itemStatus",
+                         "value": "COMPLETED"    
+                     }
+                    },
+                    {
+                     "type": "details",
+                     "layerTitleAttribute" : "name"
+                    },
+                    {
+                     "type": "delete",
+                     "layerNameAttribute" : "layerName",
+                     "idAttribute" : "fid",
+                     "wsNameAttribute" : "wsName"
+                    }
+                ]
+                
             },{
                 "ptype": "gxp_addlayer",
 		"id": "addlayer"
